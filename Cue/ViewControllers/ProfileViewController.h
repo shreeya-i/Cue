ae5c546451a2483ee7b0f1ff591f291a66259174
Shreeya Indap <shreeyaindap@fb.com>
2022-07-13 15:51:16 -0700

    [Profile][Tap gesture to change profile pic]

--- Cue/ViewControllers/ProfileViewController.h
+++ Cue/ViewControllers/ProfileViewController.h
@@ -12,18 +12,21 @@ NS_ASSUME_NONNULL_BEGIN
 
 @protocol EditDelegate
 
-- (void)didSaveEdits:(NSString *)name :(NSString *)username :(NSString *)password;
+- (void)didSaveEdits:(NSString *)name :(NSString *)username :(NSString *)password :(NSString *)address :(PFFileObject *)profileImage;
 
 @end
 
 @interface ProfileViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UIImageView *profilePicture;
+@property (weak, nonatomic) IBOutlet PFImageView *profilePicture;
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UITextField *addressField;
 @property (weak, nonatomic) IBOutlet UILabel *currentName;
 @property (weak, nonatomic) IBOutlet UILabel *currentUsername;
 @property (nonatomic, weak) id<EditDelegate> delegate;
+
++ (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image;
 
 @end
 
--- Cue/ViewControllers/ProfileViewController.m
+++ Cue/ViewControllers/ProfileViewController.m
@@ -18,6 +18,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    UITapGestureRecognizer *postTapGestureRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(didTapPlaceholderImage:)];
+    [self.profilePicture addGestureRecognizer:postTapGestureRecognizer];
+    [self.profilePicture setUserInteractionEnabled:YES];
+    
     [self fetchDetails];
 }
 
@@ -29,13 +33,76 @@
     self.profilePicture.clipsToBounds = YES;
     self.profilePicture.layer.borderWidth = 0.5f;
     self.profilePicture.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    
+    PFUser *user = [PFUser currentUser];
+    if(user[@"profilePicture"]){
+            PFFileObject *file = user[@"profilePicture"];
+            [file getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
+                        if (!error) {
+                            UIImage *image = [UIImage imageWithData:imageData];
+                            [self.profilePicture setImage:image];
+                        }
+                    }];
+    }
+    else {
+        self.profilePicture.image = [UIImage imageNamed: @"defaultpfp"];
+    }
 }
+
+- (void) didTapPlaceholderImage:(UITapGestureRecognizer *)sender{
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    if (editedImage){
+        [self.profilePicture setImage: editedImage];
+        PFFileObject *file = [self getPFFileFromImage: editedImage];
+        [self.profilePicture setFile: file];
+    }
+    else{
+        self.profilePicture.image = originalImage;
+        PFFileObject *file = [self getPFFileFromImage: originalImage];
+        [self.profilePicture setFile: file];
+    }
+    
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (PFFileObject *)getPFFileFromImage: (UIImage * _Nullable)image {
+    if (!image) {
+        return nil;
+    }
+    NSData *imageData = UIImagePNGRepresentation(image);
+    if (!imageData) {
+        return nil;
+    }
+    return [PFFileObject fileObjectWithName:@"image.png" data:imageData];
+}
+
 
 - (IBAction)didTapSave:(id)sender {
     
     NSString *nameData;
     NSString *usernameData;
     NSString *passwordData;
+    NSString *addressData;
     
     PFUser *user = [PFUser currentUser];
     if(![self.nameField.text isEqual: @""]){
@@ -54,16 +121,24 @@
         passwordData = self.passwordField.text;
         user[@"password"] = passwordData;
     }
+    if(![self.addressField.text isEqual: @""]){
+        addressData = self.addressField.text;
+        user[@"address"] = addressData;
+    }
+    if(self.profilePicture.file){
+        user[@"profilePicture"] = self.profilePicture.file;
+    }
     [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
         if(error){
             NSLog(@"Error saving: %@", error.localizedDescription);
         }
         else{
-            [self.delegate didSaveEdits: nameData :usernameData :passwordData];
+            [self.delegate didSaveEdits: nameData :usernameData :passwordData :addressData :self.profilePicture.file];
 
             self.nameField.text = @"";
             self.usernameField.text = @"";
             self.passwordField.text = @"";
+            self.addressField.text = @"";
             [self fetchDetails];
             NSLog(@"Successfully saved");
         }
